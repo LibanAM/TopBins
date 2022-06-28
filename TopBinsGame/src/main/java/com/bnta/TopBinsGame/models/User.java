69e0d56895d8322e1cea913ae245270529375be2
Jack Wells <jackwellsjw@yahoo.com>
2022-06-28 10:40:32 +0100

    Users Class made

--- TopBinsGame/src/main/java/com/bnta/TopBinsGame/models/User.java
+++ TopBinsGame/src/main/java/com/bnta/TopBinsGame/models/User.java
@@ -1,7 +1,99 @@
 package com.bnta.TopBinsGame.models;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-    Long id;
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String emailAddress;
+
+    @Column
+    private String password;
+
+    @Column
+    private int score;
+    @Column
+    private Boolean isAdmin;
+
+
+    public User(String name, String emailAddress, String password, int score, boolean isAdmin) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.score = score;
+        this.isAdmin = isAdmin;
+    }
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                ", score=" + score +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
 }
 
