0a88f16dd741d700615682a4f91839475cc282fa
Liban Mohamed <libanam7@gmail.com>
2022-06-28 09:49:51 +0100

    Added backend to repo

---
+++ TopBinsGame/src/main/java/com/bnta/TopBinsGame/TopBinsGameApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.TopBinsGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopBinsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopBinsGameApplication.class, args);
+	}
+
+}
---
+++ TopBinsGame/src/test/java/com/bnta/TopBinsGame/TopBinsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.TopBinsGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopBinsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
