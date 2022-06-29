8fe7ada903f1975cf3af734df925b404888cc05c
Liban Mohamed <libanam7@gmail.com>
2022-06-29 10:59:56 +0100

    Updated config class

--- TopBinsGame/src/main/java/com/bnta/TopBinsGame/configurations/SpringGlobalConfig.java
+++ TopBinsGame/src/main/java/com/bnta/TopBinsGame/configurations/SpringGlobalConfig.java
@@ -1,4 +1,15 @@
 package com.bnta.TopBinsGame.configurations;
 
-public class SpringGlobalConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
 }
