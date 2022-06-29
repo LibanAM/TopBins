9f42bb6a7d438a28eca9e3da41f15ac7b3db6b2f
Liban Mohamed <libanam7@gmail.com>
2022-06-29 09:57:25 +0100

    Updated & completed playercontroller

--- TopBinsGame/src/main/java/com/bnta/TopBinsGame/repositories/PlayerRepository.java
+++ TopBinsGame/src/main/java/com/bnta/TopBinsGame/repositories/PlayerRepository.java
@@ -3,6 +3,9 @@ package com.bnta.TopBinsGame.repositories;
 import com.bnta.TopBinsGame.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findByNameIgnoreCase(String name);
 
 }
