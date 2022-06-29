227409a753f7e1b0995f3985bb66b06f199cb1b8
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-06-29 16:06:37 +0100

    Created basic form for login page

--- topbins-frontend/src/containers/Register.js
+++ topbins-frontend/src/containers/Register.js
@@ -1,33 +1,36 @@
+import React from 'react';
 import './Form.css';
+
 const Register = () => {
 
-
   return (
-    <div className="form-container">
-      <form className="register-form">
-        <input
-          className="form-field"
-          type="text"
-          placeholder="Name"
-          name="Name"
-        />
-        <input
-          className="form-field"
-          type="text"
-          placeholder="Email"
-          name="email"
-        />
-        <input
-          className="form-field"
-          type="password"
-          placeholder="Password"
-          name="password"
-        />
-        <label> <input type="checkbox" name="termsCheckbox"/> I agree to the Terms and Conditions</label>
-        <button class="form-field" type="submit">
-          Register
-        </button>
-      </form>
+    <div>
+      <h1>Register</h1>
+      <div className="form-container">
+        <img src="" alt="" ></img>
+        <form className="register-form">
+          <input
+            className="form-field"
+            type="text"
+            placeholder="Name"
+            name="Name"
+          />
+          <input
+            className="form-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          <input
+            className="form-field"
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <label> <input type="checkbox" name="termsCheckbox" /> I agree to the Terms and Conditions</label>
+          <button class="form-field" type="submit"> Register </button>
+        </form>
+      </div>
     </div>
   );
 }
--- topbins-frontend/src/containers/SignIn.js
+++ topbins-frontend/src/containers/SignIn.js
@@ -1,7 +1,37 @@
+import React from 'react';
+import './Form.css';
+
 const SignIn = () => {
 
   return (
-    <></>
+    <div>
+      <h1>Sign In</h1>
+      <div className="form-container">
+        <img src="" alt="" ></img>
+        <form className="form">
+          <input
+            className="form-field"
+            type="text"
+            placeholder="Name"
+            name="Name"
+          />
+          <input
+            className="form-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          <input
+            className="form-field"
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <label> <input type="checkbox" name="termsCheckbox" /> I agree to the Terms and Conditions</label>
+          <button class="form-field" type="submit"> Sign In</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
