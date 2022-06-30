dce4b41912abad72e21999d16ee1f943bdf72306
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-06-30 16:23:39 +0100

    Added delete function to account page

--- topbins-frontend/src/containers/Account.js
+++ topbins-frontend/src/containers/Account.js
@@ -1,9 +1,44 @@
 import { useState, useEffect } from "react";
 import './Account.css';
+import { useNavigate } from 'react-router-dom';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 const Account = ({ currentAcc }) => {
 
+  const navigate = useNavigate();
+
   const [users, setUsers] = useState([]);
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+
+    confirmAlert({
+      message:"Are you sure you want to delete your account?",
+      buttons:[
+        {
+          label: "Yes",
+          onClick: () => {
+            fetch(`http://localhost:8080/users/remove/${currentAcc.id}`, 
+            {
+              method: "DELETE",
+              headers: { "Content-Type": "application/json" }
+            })
+            setUsers(users.filter(user => user.id !== currentAcc.id))
+            navigate('/signIn')
+          }
+        },
+        {
+          label: "No",
+          onClick: () => {}
+        }
+      ]
+    })
+
+
+
+
+  }
 
   return (
     <div>
@@ -26,8 +61,9 @@ const Account = ({ currentAcc }) => {
             {currentAcc.score}
           </li>
         </ul>
+        <br/>
         <button className="edit-btn">Edit</button>
-        <button className="delete-btn">Delete</button>
+        <button className="delete-btn" onClick={handleDelete}>Delete</button>
       </div>
     </div>
   )
